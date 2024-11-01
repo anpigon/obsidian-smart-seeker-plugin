bcfd417276b603700026d1d0e7a9a29dc1360f9e
anpigon <markan82@gmail.com>
2024-11-02 00:07:24 +0900

    refactor(main.ts): update import order and add type annotation for handleNoteCreateOrUpdate feat(main.ts): introduce MARKDOWN_EXTENSION constant and improve event handling scope to workspace layout ready fix(main.ts): add error handling in handleNoteCreateOrUpdate and handleNoteDelete methods

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { Plugin, TFile, TAbstractFile } from "obsidian";
+import { Plugin, TAbstractFile, TFile } from "obsidian";
 import { SettingTab } from "./settingTab";
-import { PluginSettings, DEFAULT_SETTINGS } from "./settings";
+import { DEFAULT_SETTINGS, PluginSettings } from "./settings";
+
 
 export default class MyPlugin extends Plugin {
+	private readonly MARKDOWN_EXTENSION = "md";
+
 	settings: PluginSettings;
 
 	async onload() {
@@ -11,16 +14,24 @@ export default class MyPlugin extends Plugin {
 		// 설정 탭 추가
 		this.addSettingTab(new SettingTab(this.app, this));
 
-		// 노트 생성, 업데이트, 삭제 이벤트 감지
-		this.registerEvent(
-			this.app.vault.on("create", this.handleNoteCreateOrUpdate.bind(this))
-		);
-		this.registerEvent(
-			this.app.vault.on("modify", this.handleNoteCreateOrUpdate.bind(this))
-		);
-		this.registerEvent(
-			this.app.vault.on("delete", this.handleNoteDelete.bind(this))
-		);
+		this.app.workspace.onLayoutReady(() => {
+			// 노트 생성, 업데이트, 삭제 이벤트 감지
+			this.registerEvent(
+				this.app.vault.on(
+					"create",
+					this.handleNoteCreateOrUpdate.bind(this)
+				)
+			);
+			this.registerEvent(
+				this.app.vault.on(
+					"modify",
+					this.handleNoteCreateOrUpdate.bind(this)
+				)
+			);
+			this.registerEvent(
+				this.app.vault.on("delete", this.handleNoteDelete.bind(this))
+			);
+		});
 	}
 
 	async loadSettings() {
@@ -35,16 +46,26 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	async handleNoteCreateOrUpdate(file: TAbstractFile) {
-		if (file instanceof TFile && file.extension === "md") {
-			// 노트 생성 또는 업데이트 시 파인콘DB에 저장
-			console.log(`Note created or updated: ${file.path}`);
-			// 파인콘DB에 저장하는 로직 추가
+	async handleNoteCreateOrUpdate(file: TAbstractFile): Promise<void> {
+		try {
+			if (
+				file instanceof TFile &&
+				file.extension === this.MARKDOWN_EXTENSION
+			) {
+				// 노트 생성 또는 업데이트 시 파인콘DB에 저장
+				console.log(`Note created or updated: ${file.path}`);
+				// 파인콘DB에 저장하는 로직 추가
+			}
+		} catch (error) {
+			console.error("노트 처리 중 오류 발생:", error);
 		}
 	}
 
-	async handleNoteDelete(file: TAbstractFile) {
-		if (file instanceof TFile && file.extension === "md") {
+	async handleNoteDelete(file: TAbstractFile): Promise<void> {
+		if (
+			file instanceof TFile &&
+			file.extension === this.MARKDOWN_EXTENSION
+		) {
 			// 노트 삭제 시 파인콘DB에서 삭제
 			console.log(`Note deleted: ${file.path}`);
 			// 파인콘DB에서 삭제하는 로직 추가
