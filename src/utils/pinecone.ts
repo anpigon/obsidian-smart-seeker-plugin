daf4963d4efe49670202cda9218d07f6eb4d4205
anpigon <markan82@gmail.com>
2024-11-02 18:24:45 +0900

    feat(pinecone.ts): add support for custom fetch API in Pinecone client initialization

--- src/utils/pinecone.ts
+++ src/utils/pinecone.ts
@@ -1,5 +1,9 @@
 import { Pinecone } from "@pinecone-database/pinecone";
+import obsidianFetchApi from "./obsidianFetchApi";
 
 export const createPineconeClient = (apiKey: string) => {
-	return new Pinecone({ apiKey });
+	return new Pinecone({
+		apiKey,
+		fetchApi: obsidianFetchApi,
+	});
 };
