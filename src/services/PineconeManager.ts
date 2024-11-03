9982bc726cb4a1a86d828d7503bf56b34a29b6ee
anpigon <markan82@gmail.com>
2024-11-03 15:13:27 +0900

    refactor(PineconeManager.ts): update import path for obsidianFetchApi to match project structure changes

--- src/services/PineconeManager.ts
+++ src/services/PineconeManager.ts
@@ -1,5 +1,5 @@
 import { Pinecone } from "@pinecone-database/pinecone";
-import obsidianFetchApi from "./obsidianFetchApi";
+import obsidianFetchApi from "src/utils/obsidianFetchApi";
 
 export const createPineconeClient = (apiKey: string) => {
 	return new Pinecone({
