d1e9fcdde21e0b4803e006dcbc5e97e2a956a44a
anpigon <markan82@gmail.com>
2024-11-08 23:12:47 +0900

    refactor(main.ts): update note token count calculation and add logging

--- src/main.ts
+++ src/main.ts
@@ -179,8 +179,10 @@ export default class SmartSeekerPlugin extends Plugin {
 			this.logger.info(`Note created or updated: ${file.path}`);
 			const pageContent = await this.app.vault.read(file);
 
-			// TODO: 노트의 토큰 수 계산하여 200자 미만인 경우는 제외한다.
-			const tokenCount = pageContent.split(/\s+/).length;
+			// TODO: 노트의 토큰 수 계산하여 200 토큰 미만인 경우는 제외한다.
+			const encoder = new TextEncoder();
+			const tokenCount = encoder.encode(pageContent).length;
+			console.log("tokenCount", tokenCount);
 			if (tokenCount < 200) {
 				this.logger.info(
 					`Note skipped due to insufficient tokens: ${tokenCount}`
