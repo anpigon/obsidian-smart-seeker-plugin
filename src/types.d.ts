c7fbdddab438080dd17d98f982b2a1fcd4f81c10
anpigon <markan82@gmail.com>
2024-12-01 01:31:28 +0900

    feat(types.d.ts): add types for omnisearch API to support new features and improve type safety refactor(types.d.ts): update existing types to match new API structure and improve readability

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,26 +1,35 @@
 // Add this type declaration at the top of the file or in a separate .d.ts file
 
-interface OmniSearchMatch {
+// ref: https://publish.obsidian.md/omnisearch/Public+API+%26+URL+Scheme#Omnisearch+API
+type OmniSearchMatchApi = {
 	match: string;
 	offset: number;
-}
+};
 
-interface OmniSearchResult {
+type OmniSearchResultNoteApi = {
 	score: number;
 	vault: string;
 	path: string;
 	basename: string;
 	foundWords: string[];
-	matches: OmniSearchMatch[];
+	matches: OmniSearchMatchApi[];
 	excerpt: string;
-}
+};
 
-export {}; // This makes the file a module
-
+type OmnisearchApi = {
+	// Returns a promise that will contain the same results as the Vault modal
+	search: (query: string) => Promise<OmniSearchResultNoteApi[]>;
+	// Refreshes the index
+	refreshIndex: () => Promise<void>;
+	// Register a callback that will be called when the indexing is done
+	registerOnIndexed: (callback: () => void) => void;
+	// Unregister a callback that was previously registered
+	unregisterOnIndexed: (callback: () => void) => void;
+};
 declare global {
 	interface Window {
-		omnisearch?: {
-			search?: (query: string) => Promise<OmniSearchResult[]>;
-		};
+		omnisearch?: OmnisearchApi;
 	}
 }
+
+export { }; // This makes the file a module
