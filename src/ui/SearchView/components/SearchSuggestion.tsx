078e51ecf114f38533da16214e030cf0386f18e3
anpigon <markan82@gmail.com>
2024-12-10 22:20:14 +0900

    refactor(SearchSuggestion.tsx): update icon component structure and remove unused options style(SearchSuggestion.tsx): clean up code by removing commented out options

--- src/ui/SearchView/components/SearchSuggestion.tsx
+++ src/ui/SearchView/components/SearchSuggestion.tsx
@@ -8,48 +8,59 @@ const SearchSuggestion = ({ style }: SearchSuggestionProps) => {
 			type: "group",
 			title: "검색 옵션",
 			icon: (
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					className="svg-icon lucide-info"
+				<div
+					className="list-item-part search-suggest-icon clickable-icon"
+					aria-label="더보기"
+					onClick={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						console.log("click");
+						open("https://help.obsidian.md/Plugins/Search");
+					}}
 				>
-					<circle cx="12" cy="12" r="10" />
-					<path d="M12 16v-4" />
-					<path d="M12 8h.01" />
-				</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						className="svg-icon lucide-info"
+					>
+						<circle cx="12" cy="12" r="10" />
+						<path d="M12 16v-4" />
+						<path d="M12 8h.01" />
+					</svg>
+				</div>
 			),
 		},
-		{
-			title: "path:",
-			description: "파일 경로 일치",
-		},
-		{
-			title: "file:",
-			description: "파일 이름과 일치",
-		},
+		// {
+		// 	title: "path:",
+		// 	description: "파일 경로 일치",
+		// },
+		// {
+		// 	title: "file:",
+		// 	description: "파일 이름과 일치",
+		// },
 		{
 			title: "tag:",
 			description: "태그 검색",
 		},
-		{
-			title: "line:",
-			description: "동일한 행에서 키워드 검색",
-		},
-		{
-			title: "section:",
-			description: "동일한 제목으로 키워드 검색",
-		},
-		{
-			title: "[property]",
-			description: "match property",
-		},
+		// {
+		// 	title: "line:",
+		// 	description: "동일한 행에서 키워드 검색",
+		// },
+		// {
+		// 	title: "section:",
+		// 	description: "동일한 제목으로 키워드 검색",
+		// },
+		// {
+		// 	title: "[property]",
+		// 	description: "match property",
+		// },
 	];
 
 	return (
@@ -79,14 +90,7 @@ const SearchSuggestion = ({ style }: SearchSuggestionProps) => {
 							</div>
 						</div>
 						{suggestion.icon && (
-							<div className="suggestion-aux">
-								<div
-									className="list-item-part search-suggest-icon clickable-icon"
-									aria-label="더보기"
-								>
-									{suggestion.icon}
-								</div>
-							</div>
+							<div className="suggestion-aux">{suggestion.icon}</div>
 						)}
 					</div>
 				))}
