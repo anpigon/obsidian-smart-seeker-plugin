40372bf0276d64dcddf04860f812d5c500837754
anpigon <markan82@gmail.com>
2024-12-25 21:19:25 +0900

    feat(relatedNotes): use IconRefresh component for refresh button refactor(icons): create IconRefresh component and remove inline svg refactor(icons): create IconRefreshCW component for refresh-cw icon

--- src/features/relatedNotes/components/RelatedNotes.tsx
+++ src/features/relatedNotes/components/RelatedNotes.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/shared/utils/editor/editorHelpers";
 import truncateContent from "@/shared/utils/truncateContent";
 import SearchResultItem from "@/widgets/SearchResultItem";
+import IconRefresh from "@/widgets/icons/IconRefresh";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Notice, TFile } from "obsidian";
 import { useEffect, useMemo, useRef } from "react";
@@ -215,24 +216,13 @@ const RelatedNotes = ({ currentFile }: RelatedNotesProps) => {
 					<div className="tree-item-inner">Related Note Chunks</div>
 					<div className="tree-item-flair-outer">
 						<div className="tree-item-flair">
+							{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
 							<div
 								className="clickable-icon"
 								aria-label="Refresh"
 								onClick={() => refetch()}
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								>
-									<path d="M21.5 2v6h-6M21.34 15.57a10 10 0 1 1-.57-8.38" />
-								</svg>
+								<IconRefresh />
 							</div>
 						</div>
 					</div>
